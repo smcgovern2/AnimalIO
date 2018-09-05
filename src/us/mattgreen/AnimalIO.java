c6f4106880c103ef2612f60b59ce652532ca25e7
seanonthelawn <smcgovern2@my.wctc.edu>
2018-09-04 20:49:10 -0500

    Javadoc Documentation

--- src/us/mattgreen/AnimalIO.java
+++ src/us/mattgreen/AnimalIO.java
@@ -15,14 +15,27 @@ public class AnimalIO {
         zoo = new ArrayList<Talkable>();
     }
 
+    /**
+     * A method to get the zoo arraylist from instance of animalIO
+     * @return an arraylist of talkables
+     */
     public static ArrayList<Talkable> getZoo() {
         return zoo;
     }
 
+    /**
+     * Sets the zoo arraylist
+     * @param zoo the arraylist to be assigned to this instance of animalIO
+     * @returns void
+     */
     public static void setZoo(ArrayList<Talkable> zoo) {
         AnimalIO.zoo = zoo;
     }
 
+    /**
+     * Loops through addAnimal method until prompted to stop
+     * @throws Exception inherited from contained methods
+     */
     public static void animalLoop () throws Exception{
         String continueLoop = "y";
         BufferedReader inStream = new BufferedReader(new InputStreamReader(System.in));
@@ -35,6 +48,12 @@ public class AnimalIO {
         }
     }
 
+    /**
+     * Adds animal of type passed as argument
+     * @param type the type of animal desired, (Student, Cat, or Dog)
+     * @return a Talkable object of type selected
+     * @throws Exception Illegal argument if incorrect type, otherwise inherits from contained methods
+     */
     public static Talkable addAnimal(String type) throws Exception {
         switch (type){
             case "Cat":
@@ -53,6 +72,11 @@ public class AnimalIO {
 
     }
 
+    /**
+     * A method to prompt user for info on cat and create a cat object from said info
+     * @return a cat object
+     * @throws Exception non integer value entered in mice killed
+     */
     public static Cat addCat() throws Exception {
         BufferedReader inStream = new BufferedReader(new InputStreamReader(System.in));
         Scanner scanner = new Scanner(inStream);
@@ -73,6 +97,11 @@ public class AnimalIO {
         return new Cat(miceKilled, name);
     }
 
+    /**
+     * method to add new student using data gathered from user in console
+     * @return a new student object
+     * @throws Exception when non integer entered for students age
+     */
     public static Student addStudent() throws Exception {
         BufferedReader inStream = new BufferedReader(new InputStreamReader(System.in));
         Scanner scanner = new Scanner(inStream);
@@ -93,6 +122,11 @@ public class AnimalIO {
         return new Student(age, name);
     }
 
+    /**
+     * A method to create a new dog from data gathered from user in console
+     * @return a new dog object
+     * @throws Exception when non boolean entered when asked if dog is friendly
+     */
     public static Dog addDog() throws Exception {
         BufferedReader inStream = new BufferedReader(new InputStreamReader(System.in));
         Scanner scanner = new Scanner(inStream);
